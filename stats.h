cefff25e65fdb610199654a529f00143b729f5df
sskSaravanan <ssk.saravanan08@gmail.com>
2023-05-14 19:56:42 +0530

    initial files are committed

--- stats.h
+++ stats.h
@@ -9,24 +9,16 @@
  *
  *****************************************************************************/
 /**
- * @file stats.h
- * @brief This project helps to find min, max, mean, median, stastics of given single dimensional array
+ * @file <Add File Name> 
+ * @brief <Add Brief Description Here >
  *
- * Commit1: Created empty function 
- * main() - The main entry point for your program
- * print_statistics() - A function that prints the statistics of an array including minimum, maximum, mean, and median.
- * print_array() -  Given an array of data and a length, prints the array to the screen
- * find_median() - Given an array of data and a length, returns the median value
- * find_mean() -  Given an array of data and a length, returns the mean
- * find_maximum() -  Given an array of data and a length, returns the maximum
- * find_minimum() -  Given an array of data and a length, returns the minimum
- * sort_array() - Given an array of data and a length, sorts the array from largest to smallest.  (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+ * <Add Extended Description Here>
  *
- * @author Saravanakumar Shunmugam
- * @date 14May2023
+ * @author <Add FirsName LastName>
+ * @date <Add date >
  *
  */
- #ifndef __STATS_H__
+#ifndef __STATS_H__
 #define __STATS_H__
 
 /* Add Your Declarations and Function Comments here */ 
