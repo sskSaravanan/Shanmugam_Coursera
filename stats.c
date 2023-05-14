305478f90e983510e9f5ee72acdee20f95c7cf6f
sskSaravanan <ssk.saravanan08@gmail.com>
2023-05-14 20:35:52 +0530

    function logics added for all functions

--- stats.c
+++ stats.c
@@ -36,35 +36,77 @@
 
 /* Function definations */
 void sort_array(unsigned char array[], unsigned int size) {
-      printf("\n\rShort array");
+	unsigned char i, j, a;
+	printf("\n\rShort array");
+	for (i = 0; i < size; ++i)
+	{
+		for (j = i + 1; j < size; ++j)
+		{
+		 if (array[i] > array[j])
+		 {
+			a = array[i];
+			array[i] = array[j];
+			array[j] = a;
+		 }
+		}
+	}
 }
 void print_statistics(int med, int mean, int max, int min) {
        printf("\n\rMedian = %i; \t Mean = %i; \t Maximum = %i; \t Minimum = %i\n", med, mean, max, min);
 }
 
 void print_array(unsigned char array[], unsigned int size) {
-     printf("\n\rArray list");
+	printf("\n\rArray list\n\r");
+	int i = 0;
+	for (i = 0; i < size; i++) {
+	   printf("\n\rdata[%d] = %d", i,array[i]);
+	}
 }
  
 unsigned char find_maximum(unsigned char array[], unsigned int size) {
-       unsigned char maxValue = 0;
-	   return maxValue;
+	unsigned char maxValue = array[0];
+	int i = 0;
+	for(i = 1; i < size; i++) {
+		   if(array[i] > maxValue ) {
+				   maxValue = array[i];
+		   }
+	}
+	return maxValue;
 }
 
 unsigned char find_minimum(unsigned char array[], unsigned int size) {
-       unsigned char minValue = 0;
-       return minValue;
+	unsigned char minValue = array[0];
+    int i = 0;
+	for(i = 1; i < size; i++) {
+		   if(array[i] < minValue  ) {
+				   minValue = array[i];
+		   }
+	}
+	return minValue;
 }
 
-unsigned char find_median(unsigned char array[], unsigned int size) {
-       unsigned char medianValue = 0;
-       return medianValue;
+unsigned char find_median(unsigned char sorted_array[], unsigned int size) {
+	unsigned char medianValue = 0;
+	if( size%2 == 0)                                                                  
+	{
+		medianValue = (sorted_array[(size/2)-1]+sorted_array[(size/2)])/2.0; 
+	}		
+	else                                                                           
+	{
+		medianValue = sorted_array[(size/2)]; 
+	}
+	return medianValue;
 }
  
 unsigned char find_mean(unsigned char array[], unsigned int size) {
-       unsigned char meanValue = 0;
-
-       return meanValue;
+	unsigned char meanValue = 0;
+	unsigned int sum =0;
+	int i = 0;
+	for(i = 0; i < size; i++) {
+	   sum += array[i];
+	}
+	meanValue = sum /size;
+	return meanValue;
  }
 
 void main() {
@@ -75,7 +117,18 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+  int maximum = 0, minimum = 0, median = 0, mean = 0;
+  
+  printf("\n\rCoursera Assignment 1 : Embedded software \n");
+  maximum = find_maximum(test, SIZE);
+  minimum = find_minimum(test, SIZE);
+  mean   = find_mean(test, SIZE);
+  printf("\n\rUnsorted Array list");
+  print_array(test, SIZE);
+  sort_array(test, SIZE);
+  printf("\n\rSorted array list \n");
+  print_array(test, SIZE);
+  median  = find_median(test, SIZE);
+  print_statistics(median, mean, maximum, minimum);
 
 }
